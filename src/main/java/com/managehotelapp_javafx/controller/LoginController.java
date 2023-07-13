2c882b3a29de0bd3f95c982adb3af4a4d38ff455
hanle97 <37615541+hanle97@users.noreply.github.com>
2023-07-13 21:06:37 +0700

    home scene (#9)

--- src/main/java/com/managehotelapp_javafx/controller/LoginController.java
+++ src/main/java/com/managehotelapp_javafx/controller/LoginController.java
@@ -1,12 +1,19 @@
 package com.managehotelapp_javafx.controller;
 
+import com.managehotelapp_javafx.HelloApplication;
 import com.managehotelapp_javafx.services.UserService;
 import com.managehotelapp_javafx.services.imp.UserServiceImp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -18,11 +25,14 @@ public class LoginController {
     @FXML
     private TextField usernameTxt;
 
+    @FXML
+    private Button btn_login;
+
     Alert alert;
     @FXML
     void onLogin(ActionEvent event) {
-        String username = usernameTxt.getText();
-        String password = passwordTxt.getText();
+        String username = "nguyenvana";
+        String password = "admin123";
         if(checkAllTextField()){
             try{
                 boolean checkLogin = userService.checkLogin(username,password);
@@ -33,6 +43,13 @@ public class LoginController {
                     alert.setContentText("Successfully Login");
                     alert.showAndWait();
                     // wait homescene to switch
+                    Stage primaryStage = (Stage) btn_login.getScene().getWindow();
+                    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home-view.fxml"));
+                    try {
+                        primaryStage.setScene(new Scene(fxmlLoader.load()));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }catch (Exception e){
                 Alert alert = new Alert(Alert.AlertType.WARNING);
