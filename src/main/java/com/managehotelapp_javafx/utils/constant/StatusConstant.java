c82d61129b48306f336efe7f521523fefbc9aee7
Đại Anh Dũng <101652328+daianhdung@users.noreply.github.com>
2023-07-26 10:51:14 +0700

    done sidebar, login scene (#22)

--- src/main/java/com/managehotelapp_javafx/utils/constant/StatusConstant.java
+++ src/main/java/com/managehotelapp_javafx/utils/constant/StatusConstant.java
@@ -1,7 +1,6 @@
 package com.managehotelapp_javafx.utils.constant;
 
 public class StatusConstant {
-
     public static final int BOOKING_RESERVED = 1;
     public static final int BOOKING_CHECKIN = 2;
     public static final int BOOKING_CHECKOUT = 3;
